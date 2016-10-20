8a4889267668b074bdd75a0ff2c3068d5e022a12
Matthew Brookes <mbrookes1304@gmail.com>
2016-10-20 19:07:28 +0100

    Remove restriction on rotor/plugboard file names

--- Main.cpp
+++ Main.cpp
@@ -6,35 +6,19 @@
 int main(int argc, char **argv)
 {
   std::vector<Rotor> rotorSet;
-  Plugboard plugboard;
   Reflector reflector;
 
-  std::regex rotorRegex(".*\\.rot");
-  std::regex plugboardRegex(".*\\.pb");
-  bool plugboardChosen = false;
-
-  for (int i = 1; i < argc; i++) {
-    // Iterate over each argument after program name
+  for (int i = 1; i < argc - 1; i++) {
+    // Iterate over arguments pointing to rotors
     char *arg = argv[i];
-    if (std::regex_match(arg, rotorRegex)) {
-      Rotor rotor(arg);
-      rotorSet.push_back(rotor);
-    } else if (std::regex_match(arg, plugboardRegex)) {
-      if (!plugboardChosen) {
-        plugboard = Plugboard(arg);
-        plugboardChosen = true;
-      } else {
-        std::cerr << "Plugboard already specified" << std::endl;
-        return 1;
-      }
-    }
+    Rotor rotor(arg);
+    rotorSet.push_back(rotor);
   }
-  if (!plugboardChosen) {
-    std::cerr << "Plugboard not specified" << std::endl;
-    return 1;
-  }
+
+  Plugboard plugboard(argv[argc - 1]);
 
   Enigma enigma(rotorSet, plugboard, reflector);
+
   std::string plaintext;
   for (std::string line; std::getline(std::cin >> std::ws, line);) {
     // Walk string checking all letters uppercase
